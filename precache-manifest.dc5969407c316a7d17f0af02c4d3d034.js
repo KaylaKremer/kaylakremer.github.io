d97f27f54b67c33fb686d322245920f91129a782
Kayla Kremer <kaylakremer@icloud.com>
2020-07-05 12:04:44 -0400

    Updates

--- precache-manifest.dc5969407c316a7d17f0af02c4d3d034.js
+++ precache-manifest.dc5969407c316a7d17f0af02c4d3d034.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "99338fed5f7154c5802884b2a65ba2cd",
+    "revision": "a996b838f0f2579dbdb0673e0ca977e3",
     "url": "/index.html"
   },
   {
-    "revision": "eff745f2c607ac8e8ecf",
+    "revision": "1830c1730d4cfb089a39",
     "url": "/static/css/main.5ecd60fb.chunk.css"
   },
   {
-    "revision": "3787940d84bab740e53e",
-    "url": "/static/js/2.753b2deb.chunk.js"
+    "revision": "5c2edfee5cb659429519",
+    "url": "/static/js/2.281169c2.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/static/js/2.753b2deb.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.281169c2.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "eff745f2c607ac8e8ecf",
-    "url": "/static/js/main.beb5db96.chunk.js"
+    "revision": "1830c1730d4cfb089a39",
+    "url": "/static/js/main.113b1e0a.chunk.js"
   },
   {
     "revision": "ac797b54f1d4497cdef6",
