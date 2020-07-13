1a7b15d29e65c95fa202b351a852a82aba733228
Kayla Kremer <kaylakremer@icloud.com>
2020-07-13 19:36:58 -0400

    Add new loader; Fix package.json scripts to format scss files

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -3,15 +3,15 @@ import styles from './loader.module.scss';
 
 const Loader = () => (
     <div className={styles.loader}>
-        <div className={styles.text}>
-            <span className={styles.letter}>L</span>
-            <span className={styles.letter}>O</span>
-            <span className={styles.letter}>A</span>
-            <span className={styles.letter}>D</span>
-            <span className={styles.letter}>I</span>
-            <span className={styles.letter}>N</span>
-            <span className={styles.letter}>G</span>
-        </div>
+        <div className={`${styles.triangle} ${styles.invert}`} />
+        <div className={`${styles.triangle} ${styles.invert}`} />
+        <div className={styles.triangle} />
+        <div className={`${styles.triangle} ${styles.invert}`} />
+        <div className={`${styles.triangle} ${styles.invert}`} />
+        <div className={styles.triangle} />
+        <div className={`${styles.triangle} ${styles.invert}`} />
+        <div className={styles.triangle} />
+        <div className={`${styles.triangle} ${styles.invert}`} />
     </div>
 );
 
