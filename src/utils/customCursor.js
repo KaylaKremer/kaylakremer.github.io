ef162a1c10ba69d6c6802ff6807010b7ffac1548
Kayla Kremer <kaylakremer@icloud.com>
2020-07-25 15:07:38 -0400

    Refactor customCursor.js to use GSAP

--- src/utils/customCursor.js
+++ src/utils/customCursor.js
@@ -1,75 +1,68 @@
 import { gsap } from 'gsap';
 
-const lerp = (a, b, n) => (1 - n) * a + n * b;
-const { body } = document;
-const getMousePosition = event => {
-    let e = event;
-    let posX = 0;
-    let posY = 0;
-    if (!e) {
-        e = window.event;
-    }
-    if (e.pageX || e.pageY) {
-        posX = e.pageX;
-        posY = e.pageY;
-    } else if (e.clientX || e.clientY) {
-        posX =
-            e.clientX + body.scrollLeft + document.documentElement.scrollLeft;
-        posY = e.clientY + body.scrollTop + document.documentElement.scrollTop;
-    }
-    return { x: posX, y: posY };
-};
 export default class CustomCursor {
     constructor(cursor, outerTriangle, innerTriangle) {
-        this.DOM = { cursor };
-        this.DOM.outerTriangle = outerTriangle;
-        this.DOM.innerTriangle = innerTriangle;
+        this.cursor = { cursor };
+        this.cursor.outerTriangle = outerTriangle;
+        this.cursor.innerTriangle = innerTriangle;
         this.bounds = {
-            outerTriangle: this.DOM.outerTriangle.getBoundingClientRect(),
-            innerTriangle: this.DOM.innerTriangle.getBoundingClientRect()
+            outerTriangle: this.cursor.outerTriangle.getBoundingClientRect(),
+            innerTriangle: this.cursor.innerTriangle.getBoundingClientRect()
         };
-        this.scale = 1;
-        this.opacity = 1;
-        this.mousePosition = { x: 0, y: 0 };
-        this.lastMousePosition = {
-            outerTriangle: { x: 0, y: 0 },
-            innerTriangle: { x: 0, y: 0 }
-        };
-        this.lastScale = 1;
-
-        this.initEvents();
-        this.initHovers();
-        requestAnimationFrame(() => this.render());
+        this.initCursor();
+        this.initHover();
     }
 
-    initEvents() {
+    getMousePosition = event => {
+        const { body } = document;
+        let e = event;
+        let posX = 0;
+        let posY = 0;
+        if (!e) {
+            e = window.event;
+        }
+        if (e.pageX || e.pageY) {
+            posX = e.pageX;
+            posY = e.pageY;
+        } else if (e.clientX || e.clientY) {
+            posX =
+                e.clientX +
+                body.scrollLeft +
+                document.documentElement.scrollLeft;
+            posY =
+                e.clientY + body.scrollTop + document.documentElement.scrollTop;
+        }
+        return { x: posX, y: posY };
+    };
+
+    initCursor() {
         window.addEventListener('mousemove', e => {
-            this.mousePosition = getMousePosition(e);
-            return this.mousePosition;
+            const mousePosition = this.getMousePosition(e);
+            gsap.to(this.cursor.outerTriangle, {
+                duration: 0.2,
+                x: mousePosition.x - this.bounds.outerTriangle.width / 2 - 5,
+                y: mousePosition.y - this.bounds.outerTriangle.width / 2 - 8
+            });
+            gsap.to(this.cursor.innerTriangle, {
+                duration: 0.00001,
+                x: mousePosition.x - this.bounds.innerTriangle.width,
+                y: mousePosition.y - this.bounds.innerTriangle.height
+            });
         });
     }
 
-    initHovers() {
+    initHover() {
         const handleMouseEnter = e => {
-            const target = e.currentTarget;
-            const box = target.getBoundingClientRect();
-            this.outerCursorOriginals = {
-                width: this.bounds.outerTriangle.width,
-                height: this.bounds.outerTriangle.height
-            };
-            gsap.to(this.DOM.outerTriangle, {
-                width: this.DOM.innerTriangle.width,
-                height: this.DOM.innerTriangle.height,
-                border: '1px solid red',
+            // const target = e.currentTarget;
+            gsap.to(this.cursor.outerTriangle, {
+                scale: 2,
                 duration: 0.2
             });
         };
 
         const handleMouseLeave = () => {
-            gsap.to(this.DOM.outerTriangle, {
-                width: this.outerCursorOriginals.width,
-                height: this.outerCursorOriginals.height,
-                border: 'none',
+            gsap.to(this.cursor.outerTriangle, {
+                scale: 1,
                 duration: 0.2
             });
         };
@@ -79,32 +72,5 @@ export default class CustomCursor {
             item.addEventListener('mouseenter', handleMouseEnter);
             item.addEventListener('mouseleave', handleMouseLeave);
         });
-    }
-
-    render() {
-        this.lastMousePosition.outerTriangle.x = lerp(
-            this.lastMousePosition.outerTriangle.x,
-            this.mousePosition.x - this.bounds.outerTriangle.width / 2 - 5,
-            0.25
-        );
-        this.lastMousePosition.outerTriangle.y = lerp(
-            this.lastMousePosition.outerTriangle.y,
-            this.mousePosition.y - this.bounds.outerTriangle.height / 2 - 8,
-            0.25
-        );
-        this.lastMousePosition.innerTriangle.x = lerp(
-            this.lastMousePosition.innerTriangle.x,
-            this.mousePosition.x - this.bounds.innerTriangle.width,
-            0.5
-        );
-        this.lastMousePosition.innerTriangle.y = lerp(
-            this.lastMousePosition.innerTriangle.y,
-            this.mousePosition.y - this.bounds.innerTriangle.height,
-            0.5
-        );
-        this.lastScale = lerp(this.lastScale, this.scale, 0.4);
-        this.DOM.outerTriangle.style.transform = `translateX(${this.lastMousePosition.outerTriangle.x}px) translateY(${this.lastMousePosition.outerTriangle.y}px)`;
-        this.DOM.innerTriangle.style.transform = `translateX(${this.lastMousePosition.innerTriangle.x}px) translateY(${this.lastMousePosition.innerTriangle.y}px) scale(${this.lastScale})`;
-        requestAnimationFrame(() => this.render());
     }
 }
