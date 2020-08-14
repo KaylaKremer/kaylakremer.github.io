f737694104d15b626a17afc159401b663e09a9a8
Kayla Kremer <kaylakremer@icloud.com>
2020-08-14 02:58:05 -0400

    Finish styling of border animation on buttons; Adjust eslint rule for class constructors

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,31 +1,68 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import styles from './button.module.scss';
 
-const Button = ({
-    type,
-    icon = null,
-    onClick = null,
-    ariaLabel = null,
-    children
-}) => (
-    <button
-        type={type}
-        className={`
-        ${icon ? styles[`${icon}`] : styles.button} 
-        ${type === 'submit' ? styles.submit : null} 
-        ${type === 'reset' ? styles.reset : null} 
-        cursor-button`}
-        onClick={onClick}
-        aria-label={ariaLabel}
-    >
-        <div className="border" />
-        <div className="border" />
-        <div className="border" />
-        <div className="border" />
-        {children}
-    </button>
-);
+class Button extends Component {
+    state = {
+        hover: false
+    };
+
+    toggleHover = () => {
+        this.setState(prevState => ({
+            hover: !prevState.hover
+        }));
+    };
+
+    render() {
+        const {
+            type,
+            icon = null,
+            onClick = null,
+            ariaLabel = null,
+            children
+        } = this.props;
+        const { hover } = this.state;
+        return (
+            <button
+                type={type}
+                className={`
+                    ${icon ? styles[`${icon}`] : styles.button} 
+                    ${type === 'submit' ? styles.submit : null} 
+                    ${type === 'reset' ? styles.reset : null} 
+                    cursor-button
+                `}
+                onClick={onClick}
+                aria-label={ariaLabel}
+                onMouseEnter={this.toggleHover}
+                onMouseLeave={this.toggleHover}
+                onFocus={this.play}
+                onBlur={this.pause}
+            >
+                <div
+                    className={`${styles.border} ${styles.border1} ${
+                        hover ? `${styles.play1}` : `${styles.pause}`
+                    }`}
+                />
+                <div
+                    className={`${styles.border} ${styles.border2} ${
+                        hover ? `${styles.play2}` : `${styles.pause}`
+                    }`}
+                />
+                <div
+                    className={`${styles.border} ${styles.border3} ${
+                        hover ? `${styles.play3}` : `${styles.pause}`
+                    }`}
+                />
+                <div
+                    className={`${styles.border} ${styles.border4} ${
+                        hover ? `${styles.play4}` : `${styles.pause}`
+                    }`}
+                />
+                {children}
+            </button>
+        );
+    }
+}
 
 Button.prototypes = {
     type: PropTypes.string,
