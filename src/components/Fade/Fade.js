290e5fc6598f36a814a05656b8f6de189189369a
Kayla Kremer <kaylakremer@icloud.com>
2020-08-16 16:11:47 -0400

    Make Fade component accept props to easily adjust timeout, duration, and ease values

--- src/components/Fade/Fade.js
+++ src/components/Fade/Fade.js
@@ -2,26 +2,33 @@ import React, { Component, createRef } from 'react';
 import { Transition } from 'react-transition-group';
 import { connect } from 'react-redux';
 import gsap from 'gsap';
+import PropTypes from 'prop-types';
 
-const Fade = ({ modal, children }) => (
+const Fade = ({
+    modal,
+    timeout = 1000,
+    duration = 0.5,
+    ease = 'sine.in',
+    children
+}) => (
     <Transition
-        timeout={1000}
+        timeout={timeout}
         in={modal}
         mountOnEnter
         unmountOnExit
         addEndListener={(node, done) => {
             if (modal) {
                 gsap.from(node, {
+                    duration,
                     autoAlpha: 0,
-                    ease: 'sine.in',
-                    duration: 0.5,
+                    ease,
                     onComplete: done
                 });
             } else {
                 gsap.to(node, {
+                    duration,
                     autoAlpha: 0,
-                    ease: 'sine.out',
-                    duration: 0.5,
+                    ease,
                     onComplete: done
                 });
             }
@@ -34,5 +41,13 @@ const Fade = ({ modal, children }) => (
 const mapStateToProps = state => ({
     modal: state.contact.modal
 });
+
+Fade.propTypes = {
+    modal: PropTypes.bool,
+    timeout: PropTypes.number,
+    duration: PropTypes.number,
+    ease: PropTypes.string,
+    children: PropTypes.node
+};
 
 export default connect(mapStateToProps, null)(Fade);
