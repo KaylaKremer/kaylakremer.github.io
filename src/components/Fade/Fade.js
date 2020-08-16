cef62da784313fd371fdefeb50eabad4ca3b0d22
Kayla Kremer <kaylakremer@icloud.com>
2020-08-16 15:57:08 -0400

    Clean up code in Fade.js

--- src/components/Fade/Fade.js
+++ src/components/Fade/Fade.js
@@ -3,51 +3,33 @@ import { Transition } from 'react-transition-group';
 import { connect } from 'react-redux';
 import gsap from 'gsap';
 
-class Fade extends Component {
-    fadeRef = createRef();
-
-    // componentDidMount() {
-    //     gsap.from(this.fadeRef.current, {
-    //         autoAlpha: 0,
-    //         ease: 'sine.in',
-    //         duration: 0.5
-    //     });
-    // }
-
-    render() {
-        // const { children } = this.props;
-        // return <div ref={this.fadeRef}>{children}</div>;
-        const { modal, children } = this.props;
-
-        return (
-            <Transition
-                timeout={1000}
-                in={modal}
-                mountOnEnter
-                unmountOnExit
-                addEndListener={(node, done) => {
-                    if (modal) {
-                        gsap.from(node, {
-                            autoAlpha: 0,
-                            ease: 'sine.in',
-                            duration: 0.5,
-                            onComplete: done
-                        });
-                    } else {
-                        gsap.to(node, {
-                            autoAlpha: 0,
-                            ease: 'sine.out',
-                            duration: 0.5,
-                            onComplete: done
-                        });
-                    }
-                }}
-            >
-                <div className="test">{children}</div>
-            </Transition>
-        );
-    }
-}
+const Fade = ({ modal, children }) => (
+    <Transition
+        timeout={1000}
+        in={modal}
+        mountOnEnter
+        unmountOnExit
+        addEndListener={(node, done) => {
+            if (modal) {
+                gsap.from(node, {
+                    autoAlpha: 0,
+                    ease: 'sine.in',
+                    duration: 0.5,
+                    onComplete: done
+                });
+            } else {
+                gsap.to(node, {
+                    autoAlpha: 0,
+                    ease: 'sine.out',
+                    duration: 0.5,
+                    onComplete: done
+                });
+            }
+        }}
+    >
+        {children}
+    </Transition>
+);
 
 const mapStateToProps = state => ({
     modal: state.contact.modal
